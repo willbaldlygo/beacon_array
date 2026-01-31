ac5968a5f2d08f7f30b3e37fb5debfbe7439bba7
Will Roberts <will@baldlygo.info>
2026-01-31 20:50:08 +0000

    Initial Commit

---
+++ Beacon/BeaconApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeaconApp.swift
+//  Beacon
+//
+//  Created by Will Roberts on 31/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct BeaconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Beacon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Beacon
+//
+//  Created by Will Roberts on 31/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
